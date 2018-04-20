69ac790c37971de7cdc0814e07090358dc012e59
Jesse Chan <jc@lineageos.org>
2018-04-20 01:26:53 -0700

    sdm845-common: remap bixby key as ASSIST key

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -2,4 +2,5 @@ def FullOTA_InstallEnd(info):
     info.script.AppendExtra('run_program("/sbin/mount", "-t", "auto", "/system");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "-e", "$akey 703 ASSIST", "/system/usr/keylayout/Generic.kl");');
     info.script.AppendExtra('run_program("/sbin/umount", "-t", "auto", "/system");');
