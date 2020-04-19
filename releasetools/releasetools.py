5f0077441dab97cc2ab424160bf404edc0bda803
klabit87 <keith.labit@gmail.com>
2020-04-19 15:22:01 -0500

    Fixup sepolicy for SAR compatibility

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,5 +1,5 @@
 def FullOTA_InstallEnd(info):
     info.script.AppendExtra('run_program("/sbin/mount", "-t", "auto", "/system");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
     info.script.AppendExtra('run_program("/sbin/umount", "-t", "auto", "/system");');
