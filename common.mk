454455b7373798e5fdfa656d78e43a1251a7d2e4
klabit87 <keith.labit@gmail.com>
2020-05-29 15:08:57 -0500

    Import light HAL from Lineageos/hardware_samsung

--- common.mk
+++ common.mk
@@ -86,7 +86,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.samsung
+    android.hardware.light@2.0-service.sdm845
 
 # Net
 PRODUCT_PACKAGES += \
