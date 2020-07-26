5ec3d21875831b32968c2d6269b572669f153be8
klabit87 <keith.labit@gmail.com>
2020-07-26 11:26:25 -0500

    Fixup releasetools scripts

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,6 +1,6 @@
 def FullOTA_InstallEnd(info):
-    info.script.AppendExtra('run_program("/sbin/mount", "-t", "auto", "/system");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system/system/etc/selinux/plat_sepolicy.cil");');
-    info.script.AppendExtra('run_program("/sbin/sed", "-i", "-e", "$akey 703 ASSIST", "/system/usr/keylayout/Generic.kl");');
-    info.script.AppendExtra('run_program("/sbin/umount", "-t", "auto", "/system");');
+    info.script.AppendExtra('run_program("/sbin/mount", "-t", "auto", "/system_root");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/exfat/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "/sdfat/d", "/system_root/system/etc/selinux/plat_sepolicy.cil");');
+    info.script.AppendExtra('run_program("/sbin/sed", "-i", "-e", "$akey 703 ASSIST", "/system_root/system/usr/keylayout/Generic.kl");');
+    info.script.AppendExtra('run_program("/sbin/umount", "-t", "auto", "/system_root");');
